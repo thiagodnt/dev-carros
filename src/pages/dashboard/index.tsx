2e65fa936d4058a7751a52f7f02a8345a6bf2802
thiagodnt <thiagodnt@hotmail.com>
2026-03-09 19:44:55 -0300

    feat: componente panel

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,3 +1,10 @@
+import { Container } from '../../components/container';
+import { Panel } from '../../components/panel';
+
 export function Dashboard() {
-	return <div>Dashboard</div>;
+	return (
+		<Container>
+			<Panel />
+		</Container>
+	);
 }
